6394f3453a4b4d35a9da317f447db52ed86ea629
Ed Fine <edward.fin@gmail.com>
2021-06-15 16:05:02 -0700

    update afternoons 3&4

---
+++ tail.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+def tail(f, num):
+    dq = deque([], maxlen=num)
+    for line in f:
+        dq.append(line)
+    for line in dq:
+        print(dq)
+
+f = open('poem.txt')
+tail(f, 3)
+f.close
